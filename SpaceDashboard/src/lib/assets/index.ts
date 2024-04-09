c610badf443d1b1723c6b1de84d3490c91d515ee
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-09 21:28:20 +0200

    Too much work for those icons

--- SpaceDashboard/src/lib/assets/index.ts
+++ SpaceDashboard/src/lib/assets/index.ts
@@ -2,5 +2,7 @@ import reddit from './reddit.svg';
 import reddit_inactive from './reddit_inactive.svg';
 import youtube from './youtube.svg';
 import youtube_inactive from './youtube_inative.svg';
+import wikipedia from './wikipedia.svg';
+import wikipedia_inactive from './wikipedia_inactive.svg';
 
-export { reddit_inactive, reddit, youtube, youtube_inactive };
+export { reddit_inactive, reddit, youtube, youtube_inactive, wikipedia,wikipedia_inactive };
