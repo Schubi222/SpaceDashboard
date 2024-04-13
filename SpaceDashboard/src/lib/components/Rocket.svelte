8177140e657bf23857d2628197f0bb5e56f38f5d
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-13 17:44:26 +0200

    added engine

--- SpaceDashboard/src/lib/components/Rocket.svelte
+++ SpaceDashboard/src/lib/components/Rocket.svelte
@@ -3,10 +3,12 @@
 
 	export let rocket: RocketsResponse;
 	export let company: CompanyResponse;
+	let showing_info: null | string = 'engine';
 </script>
 
 <div class="Wrapper">
 	<div class="heading">{rocket.name}</div>
+	{rocket.description}
 	<div class="divider">
 		<div class="left-side">
 			<div class="label">Company</div>
@@ -33,27 +35,49 @@
 			<div class="info">{rocket.diameter.meters}</div>
 			<div class="label">Height</div>
 			<div class="info">{rocket.height.meters}</div>
-			<div class="label"></div>
+			<div class="label">Engine Type</div>
 			<div class="info">{rocket.engines.type}</div>
 
-			<div class="label"></div>
+			<div class="label">First Flight</div>
 			<div class="info">{rocket.first_flight}</div>
-			<div class="label"></div>
-			<div class="info">{rocket.first_stage}</div>
-			<div class="label"></div>
-			<div class="info">{rocket.landing_legs.material} {rocket.landing_legs.number}</div>
-			<div class="label"></div>
-			<div class="info">{rocket.second_stage}</div>
-			<div class="label"></div>
-			<div class="info">{rocket.stages}</div>
-			<div class="label"></div>
-			<div class="info">{rocket.type}</div>
+			<div class="label">Landing Legs</div>
+			<div class="info">{rocket.landing_legs.number} {rocket.landing_legs.material}</div>
 		</div>
 	</div>
 	<div class="additional">
-		<button>Engine</button>
-		<button>Stage</button>
-		<button>Engine</button>
+		<button on:click={() => (showing_info = 'engine')}>Engine</button>
+		<button on:click={() => (showing_info = 'stage')}>Stage</button>
+		<button on:click={() => (showing_info = null)}>Close</button>
+	</div>
+
+	<div class="additional_info">
+		{#if showing_info === 'engine'}
+			<div class="label">Engine Type</div>
+			<div class="info">{rocket.engines.type} Version{rocket.engines.version}</div>
+			<div class="label">Specific Impulse (Sea|Vacuum)</div>
+			<div class="info">
+				<!--				TODO: DOUBLE CHECK -->
+				{rocket.engines.isp.sea_level}m/s | {rocket.engines.isp.vacuum}m/s
+			</div>
+			<div class="label">Layout</div>
+			<div class="info">{rocket.engines.layout}</div>
+			<div class="label">Engine Count</div>
+			<div class="info">{rocket.engines.number}</div>
+			<div class="label">Max Engine Loss</div>
+			<div class="info">{rocket.engines.engine_loss_max}</div>
+			<div class="label">Propellant</div>
+			<div class="info">
+				Propellant 1: {rocket.engines.propellant_1} | Propellant 2: {rocket.engines.propellant_2}
+			</div>
+			<div class="label">Thrust(Sea|Vacuum)</div>
+			<div class="info">
+				{rocket.engines.thrust_sea_level.kN}kN |{rocket.engines.thrust_vacuum.kN}kN
+			</div>
+			<div class="label">TWR</div>
+			<div class="info">{rocket.engines.thrust_to_weight}</div>
+		{:else if showing_info === 'stage'}
+			STAGE
+		{/if}
 	</div>
 </div>
 
