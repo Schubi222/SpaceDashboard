765bce1fccc6b8a00edf91942d7872018b7504cb
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-14 13:07:12 +0200

    Engine extracted into component and fully build

---
+++ SpaceDashboard/src/lib/components/Engine.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { RocketsResponse } from '$lib/types/spacex/apiResponse';
+
+	export let rocket: RocketsResponse;
+</script>
+
+<div class="label">Engine Type</div>
+<div class="info">{rocket.engines.type} {rocket.engines.version}</div>
+<div class="label">Specific Impulse (Sea|Vacuum)</div>
+<div class="info">
+	<!--				TODO: DOUBLE CHECK -->
+	{rocket.engines.isp.sea_level}m/s | {rocket.engines.isp.vacuum}m/s
+</div>
+<div class="label">Layout</div>
+<div class="info">{rocket.engines.layout}</div>
+<div class="label">Engine Count</div>
+<div class="info">{rocket.engines.number}</div>
+<div class="label">Max Engine Loss</div>
+<div class="info">{rocket.engines.engine_loss_max}</div>
+<div class="label">Propellant</div>
+<div class="info">
+	Propellant 1: {rocket.engines.propellant_1} | Propellant 2: {rocket.engines.propellant_2}
+</div>
+<div class="label">Thrust(Sea|Vacuum)</div>
+<div class="info">
+	{rocket.engines.thrust_sea_level.kN}kN | {rocket.engines.thrust_vacuum.kN}kN
+</div>
+<div class="label">TWR</div>
+<div class="info">{rocket.engines.thrust_to_weight}</div>
--- SpaceDashboard/src/lib/components/Rocket.svelte
+++ SpaceDashboard/src/lib/components/Rocket.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import type { CompanyResponse, RocketsResponse } from '$lib/types/spacex/apiResponse';
+	import Engine from '$lib/components/Engine.svelte';
 
 	export let rocket: RocketsResponse;
 	export let company: CompanyResponse;
-	let showing_info: null | string = 'engine';
+	let showing_info: null | string = 'stage';
 </script>
 
 <div class="Wrapper">
@@ -52,29 +53,7 @@
 
 	<div class="additional_info">
 		{#if showing_info === 'engine'}
-			<div class="label">Engine Type</div>
-			<div class="info">{rocket.engines.type} Version{rocket.engines.version}</div>
-			<div class="label">Specific Impulse (Sea|Vacuum)</div>
-			<div class="info">
-				<!--				TODO: DOUBLE CHECK -->
-				{rocket.engines.isp.sea_level}m/s | {rocket.engines.isp.vacuum}m/s
-			</div>
-			<div class="label">Layout</div>
-			<div class="info">{rocket.engines.layout}</div>
-			<div class="label">Engine Count</div>
-			<div class="info">{rocket.engines.number}</div>
-			<div class="label">Max Engine Loss</div>
-			<div class="info">{rocket.engines.engine_loss_max}</div>
-			<div class="label">Propellant</div>
-			<div class="info">
-				Propellant 1: {rocket.engines.propellant_1} | Propellant 2: {rocket.engines.propellant_2}
-			</div>
-			<div class="label">Thrust(Sea|Vacuum)</div>
-			<div class="info">
-				{rocket.engines.thrust_sea_level.kN}kN |{rocket.engines.thrust_vacuum.kN}kN
-			</div>
-			<div class="label">TWR</div>
-			<div class="info">{rocket.engines.thrust_to_weight}</div>
+			<Engine bind:rocket />
 		{:else if showing_info === 'stage'}
 			STAGE
 		{/if}
