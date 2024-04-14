c9244bd10054dc3dad2b4906bb85b5a329bb0612
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-14 21:49:09 +0200

    .

---
+++ SpaceDashboard/src/lib/components/Payload.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { LaunchResponse, PayloadsResponse } from '$lib/types/spacex/apiResponse';
+
+	export let payload: PayloadsResponse;
+	export let launch: LaunchResponse | undefined;
+</script>
+
+<div class="Wrapper">
+	<div class="heading">{payload.name}</div>
+	<div class="divider">
+		<div class="left-side">
+			<div class="label">Type</div>
+			<div class="info">{payload.type}</div>
+			<div class="label">Customer</div>
+			<div class="info">{payload.customers}</div>
+			<div class="label">Manufacturers</div>
+			<div class="info">{payload.manufacturers}</div>
+			<div class="label">Nationality</div>
+			<div class="info">{payload.nationalities}</div>
+			<div class="label">Launch</div>
+			{#if launch}
+				<a class="info clickable" href={'/launch/' + launch.id}>{launch.name}<span>&rarr;</span></a>
+			{:else}
+				<div class="info">There is no launch for this payload</div>
+			{/if}
+			<div class="label">Lifespan</div>
+			<div class="info">{payload.lifespan_years}</div>
+			<div class="label">Mass</div>
+			<div class="info">{payload.mass_kg} kg</div>
+			<div class="label">Orbit</div>
+			<div class="info">{payload.orbit}</div>
+			<div class="label">Reuse</div>
+			<div class="info">{payload.reused ? 'Has been reused' : 'Has not been reused'}</div>
+		</div>
+		<div class="right-side"></div>
+	</div>
+</div>
