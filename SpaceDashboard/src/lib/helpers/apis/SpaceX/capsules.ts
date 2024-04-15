362006993661692af0a3cf060446ccad3510e3b1
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-15 21:50:12 +0200

    also fix endpoint

--- SpaceDashboard/src/lib/helpers/apis/SpaceX/capsules.ts
+++ SpaceDashboard/src/lib/helpers/apis/SpaceX/capsules.ts
@@ -3,8 +3,8 @@ import type { CapsuleResponse } from '$lib/types/spacex/apiResponse';
 import { getAllOfEntity, getEntityById } from '$lib/helpers/apis/SpaceX/BaseQueries';
 
 export async function getCapsulesById(id: string) {
-	return await getEntityById<CapsuleResponse>(id, endpoints.capsule);
+	return await getEntityById<CapsuleResponse>(id, endpoints.capsules);
 }
 export async function getAllCapsules() {
-	return await getAllOfEntity<CapsuleResponse>(endpoints.capsule);
+	return await getAllOfEntity<CapsuleResponse>(endpoints.capsules);
 }
