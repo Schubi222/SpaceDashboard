e499cd00348d960fe1e539fdc3fc969c32fe07f4
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-15 22:03:13 +0200

    started with crew screen

---
+++ SpaceDashboard/src/lib/components/Crew.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { CrewResponse, LaunchResponse } from '$lib/types/spacex/apiResponse';
+	import CrewMember from '$lib/components/CrewMember.svelte';
+
+	export let crew: CrewResponse[];
+	export let launch: LaunchResponse;
+</script>
+
+<div class="Wrapper">
+	<div class="heading">Crew of {launch.name}</div>
+	<div class="row">
+		{#each crew as member, index}
+			<CrewMember bind:member role={launch.crew[index].role} />
+		{/each}
+	</div>
+</div>
---
+++ SpaceDashboard/src/lib/components/CrewMember.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { CrewResponse } from '$lib/types/spacex/apiResponse';
+
+	export let member: CrewResponse;
+	export let role: string;
+</script>
+
+<div class="Crew_Wrapper">
+	<img src={member.image} alt="Member" />
+	<div class="label">Name</div>
+	<div class="info">{member.name}</div>
+	<div class="label">Role</div>
+	<div class="info">{role}</div>
+	<div class="label">Agency</div>
+	<div class="info">{member.agency}</div>
+	<div class="label">Status</div>
+	<div class="info">{member.status}</div>
+	<div class="label">Launches</div>
+	<div class="info">{member.launches.length}</div>
+</div>
