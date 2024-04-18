6e5068994056af151a63c17baf5cdfcc7cafc270
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-18 11:37:58 +0200

    added flags

--- SpaceDashboard/src/lib/assets/index.ts
+++ SpaceDashboard/src/lib/assets/index.ts
@@ -5,6 +5,10 @@ import youtube_inactive from './youtube_inative.svg';
 import wikipedia from './wikipedia.svg';
 import wikipedia_inactive from './wikipedia_inactive.svg';
 import crew_img from './crew.svg';
+import us_flag from './us_flag.svg';
+import russia_flag from './russia_flag.svg';
+import japan_flag from './japan_flag.svg';
+import eu_flag from './eu_flag.svg';
 
 export {
 	reddit_inactive,
@@ -13,5 +17,9 @@ export {
 	youtube_inactive,
 	wikipedia,
 	wikipedia_inactive,
-	crew_img
+	crew_img,
+	us_flag,
+	russia_flag,
+	japan_flag,
+	eu_flag
 };
