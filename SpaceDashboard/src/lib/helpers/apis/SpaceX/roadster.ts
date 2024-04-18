55bc5daf460d20caaa8a74034dda5318656c3d04
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-18 11:38:23 +0200

    fixed roaster call

--- SpaceDashboard/src/lib/helpers/apis/SpaceX/roadster.ts
+++ SpaceDashboard/src/lib/helpers/apis/SpaceX/roadster.ts
@@ -2,6 +2,6 @@ import { endpoints } from '$lib/helpers/apis/SpaceX/constants';
 import type { RoadsterResponse } from '$lib/types/spacex/apiResponse';
 import { getAllOfEntity } from '$lib/helpers/apis/SpaceX/BaseQueries';
 
-export async function getAllCrew() {
+export async function getAllRoadster() {
 	return await getAllOfEntity<RoadsterResponse>(endpoints.roadster);
 }
