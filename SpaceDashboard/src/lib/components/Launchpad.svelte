4370e986bc2843362937ffe3bcd5adc857d9d3e3
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-21 19:58:24 +0200

    fix launchpad

--- SpaceDashboard/src/lib/components/Launchpad.svelte
+++ SpaceDashboard/src/lib/components/Launchpad.svelte
@@ -9,34 +9,31 @@
 <div class="Wrapper">
 	<BackBtn />
 	<div class="heading">{launchpad.full_name} ({launchpad.name})</div>
-	<div class="divider">
-		<div class="left-side">
-			<div class="label">Region | Latitude | Longitude | Timezone</div>
-			<div class="info">
-				{launchpad.locality} ({launchpad.region}) | {launchpad.latitude?.toFixed(2)}° | {launchpad.longitude?.toFixed(
-					2
-				)}° | {launchpad.timezone}
-			</div>
-			<div class="label">Launches</div>
-			<div class="info">{launchpad.launch_attempts} ({launchpad.launch_successes} successful)</div>
-			<div class="label">Success Rate</div>
-			<div class="info">
-				{((launchpad.launch_attempts / launchpad.launch_successes) * 100).toFixed(2)}%
-			</div>
-			<div class="label">Rockets</div>
-			{#if rockets}
-				<div class="info">
-					{#each rockets as rocket}
-						<a href={'/SpaceX/rocket/' + rocket.id} class="clickable">{rocket.name}</a>
-					{/each}
-				</div>
-			{:else}
-				<div class="info">No rockets in this launchpad</div>
-			{/if}
-			<div class="label">Status</div>
-			<div class="info">{launchpad.status}</div>
+	<div class="left-side">
+		<div class="label">Region | Latitude | Longitude | Timezone</div>
+		<div class="info">
+			{launchpad.locality} ({launchpad.region}) | {launchpad.latitude?.toFixed(2)}° | {launchpad.longitude?.toFixed(
+				2
+			)}° | {launchpad.timezone}
 		</div>
-		<div class="right-side"></div>
+		<div class="label">Launches</div>
+		<div class="info">{launchpad.launch_attempts} ({launchpad.launch_successes} successful)</div>
+		<div class="label">Success Rate</div>
+		<div class="info">
+			{((launchpad.launch_attempts / launchpad.launch_successes) * 100).toFixed(2)}%
+		</div>
+		<div class="label">Rockets</div>
+		{#if rockets}
+			<div class="info">
+				{#each rockets as rocket}
+					<a href={'/SpaceX/rocket/' + rocket.id} class="clickable">{rocket.name}</a>
+				{/each}
+			</div>
+		{:else}
+			<div class="info">No rockets in this launchpad</div>
+		{/if}
+		<div class="label">Status</div>
+		<div class="info">{launchpad.status}</div>
 	</div>
 </div>
 
