1c11459e1d3deb9631353bfbd2438a1923a30ac4
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2024-04-22 18:10:49 +0200

    nothing found

---
+++ SpaceDashboard/src/lib/components/NothingFound.svelte
@@ -0,0 +1,30 @@
+<div class="full-size">
+	<div class="error-wrapper">
+		<div class="heading">Nothing found!</div>
+		<div class="info">It seams like there is no data to this id.</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.full-size {
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+	}
+	.error-wrapper {
+		display: flex;
+		flex-direction: column;
+		background: white;
+		width: 300px;
+		height: 300px;
+		border-radius: 15px;
+		padding: 30px;
+		align-items: center;
+		font-weight: bold;
+		text-align: center;
+		margin: auto;
+		.heading {
+			padding-bottom: 70px;
+		}
+	}
+</style>
